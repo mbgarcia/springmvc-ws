3b2f5c57887bb105a4e714e6b5e7281ddd4a1219
Marcelo Garcia <marcelogarcia@Marcelos-MacBook-Pro.local>
2019-01-02 08:33:41 -0300

    Pagination with SpringDataJPA

--- src/main/java/com/appsdeveloperblog/app/ws/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.appsdeveloperblog.app.ws.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
@@ -68,5 +72,20 @@ public class UserController {
 	@DeleteMapping(path="/{publicId}")
 	public void deleteUser(@PathVariable String publicId){
 		userService.deleteUser(publicId);
+	}
+	
+	@GetMapping(produces={MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	public List<UserControllerResponse> getUsers(@RequestParam(defaultValue="1") int page, @RequestParam(defaultValue="10") int limit){
+		List<UserControllerResponse> list = new ArrayList<UserControllerResponse>();
+		
+		List<UserEntity> users = userService.getUsers(page, limit);
+		
+		users.stream().forEach(user -> {
+			UserControllerResponse item = new UserControllerResponse();
+			BeanUtils.copyProperties(user, item);
+			list.add(item);
+		});
+		
+		return list;
 	}
 }
--- src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
@@ -2,8 +2,11 @@ package com.appsdeveloperblog.app.ws.service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,74 +21,84 @@ import com.appsdeveloperblog.app.ws.shared.Utils;
 import com.appsdeveloperblog.app.ws.ui.model.request.UserUpdateRequestModel;
 
 @Service
-public class UserService implements UserDetailsService{
-	
+public class UserService implements UserDetailsService {
+
 	@Autowired
 	UserRepository repository;
-	
+
 	@Autowired
 	Utils utils;
-	
+
 	@Autowired
 	BCryptPasswordEncoder passwordEncoder;
 
 	public UserEntity createUser(UserEntity user) {
-		
-		if (repository.existsByEmail(user.getEmail())) throw new RuntimeException("User already exists");
-		
+
+		if (repository.existsByEmail(user.getEmail()))
+			throw new RuntimeException("User already exists");
+
 		user.setEncryptedPassword(passwordEncoder.encode(user.getPassword()));
 		user.setUserId(utils.generateUserId(20));
-		
+
 		UserEntity storedUser = repository.save(user);
-		
+
 		return storedUser;
 	}
-	
+
 	public UserEntity updateUser(String publicId, UserUpdateRequestModel user) {
 		UserEntity entity = findUserByPublicId(publicId);
-		
+
 		entity.setLastName(user.getLastName());
 		entity.setFirstName(user.getFirstName());
-		
+
 		UserEntity storedUser = repository.save(entity);
-		
+
 		return storedUser;
 	}
-	
-	public void deleteUser(String publicId){
+
+	public void deleteUser(String publicId) {
 		UserEntity entity = findUserByPublicId(publicId);
-		
+
 		entity.setDeleted(true);
 		entity.setDeletedAt(LocalDateTime.now());
-		
+
 		repository.save(entity);
 	}
-	
+
 	public UserEntity findUserByPublicId(String publicId) {
 		UserEntity user = repository.findByUserId(publicId);
-		
+
 		if (user == null)
 			throw new UserNotFoundException(publicId);
-		
+
 		return user;
-	}	
+	}
 
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		UserEntity user = repository.findByEmail(email);
-		
+
 		if (user == null)
 			throw new UsernameNotFoundException(email);
-		
+
 		return new User(user.getEmail(), user.getEncryptedPassword(), new ArrayList<>());
 	}
-	
-	public UserEntity findUserByEmail(String email){
+
+	public UserEntity findUserByEmail(String email) {
 		UserEntity user = repository.findByEmail(email);
-		
+
 		if (user == null)
 			throw new UsernameNotFoundException(email);
-		
+
 		return user;
+	}
+
+	public List<UserEntity> getUsers(int page, int limit) {
+		if (page > 0)
+			page -= 1;
+
+		Pageable pageable = PageRequest.of(page, limit);
+
+		return repository.findAll(pageable).getContent();
 	}
 }
